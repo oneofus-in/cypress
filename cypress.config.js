374c56bbd2232c22d8fde2644e3ca023782b5c1d
Bokken-jo <netanel@unichan.co>
2025-12-02 13:48:00 +0200

    first push

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://oneofus.in',
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
